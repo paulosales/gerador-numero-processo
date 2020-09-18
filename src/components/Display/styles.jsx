8a1af5d784102ea29ec43776128a83da3cc0bbda
Paulo Rogério Sales Santos <paulosales@gmail.com>
2020-09-18 15:50:25 -0300

    Responsiveness added.

--- src/components/Display/styles.jsx
+++ src/components/Display/styles.jsx
@@ -2,8 +2,11 @@ import styled from 'styled-components'
 
 export const DisplayContainer = styled.div`
   margin: 5px;
-  width: 640px;
   border-radius: 5px;
+  font-size: 2.5rem;
+  @media (max-width: 400px) {
+    font-size: 1.5rem;
+  }
 `
 
 export const DisplayLabel = styled.div`
@@ -13,7 +16,6 @@ export const DisplayLabel = styled.div`
 export const DisplayContent = styled.div`
   display: flex;
   justify-content: space-between;
-  font-size: 2.5rem;
   border-radius: 5px;
   padding: 5px;
   border: 1px solid var(--primary-border);
